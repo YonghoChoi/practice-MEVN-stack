920d57030dd55b9089a803ed53577987dc6644c1
Yongho Choi <yongho1037@gmail.com>
2019-07-02 01:02:36 +0900

    식당 리스트 추가 시 v-card를 매번 추가하는 것은 비효율적 v-for를 사용하여 data 동적 바인딩

--- fe/src/views/restaurant-list.vue
+++ fe/src/views/restaurant-list.vue
@@ -16,7 +16,17 @@
                 xs12 sm4는 flex 박스 크기를 의미 (extra-small 사이즈의 화면에서는 12를 차지하고, small 사이즈의 화면에서는 4를 차지)
                 휴대폰에서는 한 줄에 하나의 식당이 보이고, 큰 화면에서는 한줄에 4,4,4 크기를 갖는 3개의 식당이 보이게됨
             -->
-            <v-flex xs12 sm4>
+            <v-flex 
+                xs12
+                sm4
+                v-for="(restaurant, index) in restaurants"
+                :key="index">   
+                <!-- 
+                    for문 사용시 유니크한 키를 부여해야함. 이 키를 바탕으로 데이터가 변화했을 때 화면을 어떤식으로 재 렌더링 할지를 결정
+                    :key는 index라는 변수를 key에 바인딩하겠다는 의미
+                -->
+
+
                 <!-- 
                     v-card는 정보를 표현하는 자리를 만듦 
                     내부에는 제목을 표시할 v-card-title, 내용이 될 v-card-text, 기타 버튼등이 들어갈 v-card-actions로 나뉠 수 있음
@@ -24,33 +34,11 @@
                 <v-card>
                     <!-- -->
                     <v-card-title>
-                        장모님 식당
+                        {{ restaurant.name }}
                     </v-card-title>
                     <v-card-text>
-                        한식
-                        주소 : 경기도 안양시
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-            <v-flex xs12 sm4>
-                <v-card>
-                    <v-card-title>
-                        아저씨 초밥
-                    </v-card-title>
-                    <v-card-text>
-                        일식
-                        주소 : 경기도 안양시
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-            <v-flex xs12 sm4>
-                <v-card>
-                    <v-card-title>
-                        매운 짬뽕집
-                    </v-card-title>
-                    <v-card-text>
-                        중식
-                        주소 : 경기도 안양시
+                        {{ restaurant.type }}
+                        주소 : {{ restaurant.address }}
                     </v-card-text>
                 </v-card>
             </v-flex>
@@ -62,6 +50,23 @@
 export default {
     data() {
         return {
+            restaurants: [
+                {
+                    name: '장모님 식당',
+                    type: '한식',
+                    address: '경기도 안양시'
+                },
+                {
+                    name: '아저씨 초밥',
+                    type: '일식',
+                    address: '경기도 안양시'
+                },
+                {
+                    name: '매운 짬뽕집',
+                    type: '중식',
+                    address: '경기도 안양시'
+                }
+            ]
 
         }
     }
