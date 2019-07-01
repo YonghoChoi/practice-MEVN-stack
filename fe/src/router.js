00b40f9e78d5fe46a73f4f08f7ad9a8a95c75523
Yongho Choi <yongho1037@gmail.com>
2019-07-02 00:55:22 +0900

    식당 리스트 페이지로 접근하기 위한 라우터 추가

--- fe/src/router.js
+++ fe/src/router.js
@@ -4,6 +4,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import restaurantList from './views/restaurant-list.vue'
 
 Vue.use(Router)
 
@@ -17,11 +18,17 @@ export default new Router({
       component: Home // Home 컴포넌트를 명시했으므로 Home.vue를 렌더링
     },
     {
+      path: '/list',
+      name: 'restaurantList',
+      component: restaurantList
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      // import하지 않고 해당 URL로 접근 시 로딩
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
